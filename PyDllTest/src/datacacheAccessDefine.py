e33278da6ba56a8df65c2d163833001ba60a7f18
gfxmode <gfxmode@live.com>
2015-12-02 12:34:06 +0800

    Finish python2 call datacacheaccess.dll

--- PyDllTest/src/datacacheAccessDefine.py
+++ PyDllTest/src/datacacheAccessDefine.py
@@ -38,12 +38,25 @@ DATACACHE_TYPE_OTHER = 6;
 
 # 定义最大读取数
 MAX_RWLOGCNT = 100;
+# 故障录波文件最大字节数
+MAX_COMTRADE_FILELEN = 2*1024*1024
 
 '''
 数据结构定义
 '''
+class tagSYSTEMTIME(Structure):
+    _pack_ = 1;                     # 设置压缩存储
+    _fields_ =[("wYear", c_uint16),
+               ("wMonth", c_uint16),
+               ("wDayOfWeek", c_uint16),
+               ("wDay", c_uint16),
+               ("wHour", c_uint16),
+               ("wMinute", c_uint16),
+               ("wSecond", c_uint16),
+               ("wMilliseconds", c_uint16)]; 
 # 数据序号，索引块
 class tagDTSNINDEX(Structure):
+    _pack_ = 1;                     # 设置压缩存储
     _fields_ = [("logtype", c_ushort),
                 ("reserved1", c_ushort),
                 ("datfileid", c_ulong),
@@ -53,10 +66,11 @@ class tagDTSNINDEX(Structure):
                 ("loglen", c_ulong),
                 ("logcrc", c_ulong),
                 ("reserved2", c_ushort),
-                ("sumcrc", c_ushort)]
-    
+                ("sumcrc", c_ushort)];  
+# 定时记录结构体定义    
 class tagDATALOG(Structure):
-    _fields_ = [("systm", c_uint16 * 8),
+    _pack_ = 1;                     # 设置压缩存储
+    _fields_ = [("systm", tagSYSTEMTIME),
                 ("stationID", c_ulong),
                 ("channelID", c_ulong),
                 ("deviceID", c_ulong),
@@ -64,4 +78,40 @@ class tagDATALOG(Structure):
                 ("num", c_ulong),
                 ("arrPara", (c_ulong)*16),
                 ("souid", c_ulong),
-                ("arrVal", (c_double)*16)]
+                ("arrVal", (c_double)*16)];
+# 事件记录结构体定义   
+class tagEVENTLOG(Structure):
+    _pack_ = 1;                     # 设置压缩存储
+    _fields_ = [("id", c_ulong),
+                ("time", c_ulong),
+                ("msec", c_ulong),
+                ("alarm_class", c_ulong),
+                ("alarm_type", c_ulong),
+                ("alarm_byte", c_ulong),
+                ("station_id", c_ulong),
+                ("channel_id", c_ulong),
+                ("device_id", c_ulong),
+                ("station_flag", c_ubyte),
+                ("code1", c_ulong),
+                ("code2", c_ulong),
+                ("evestr1", c_char * 256),
+                ("evestr2", c_char * 256),
+                ("information", c_char * 256),
+                ("computername", c_char * 128)];
+                
+class tagWFORMLOG(Structure):
+    _pack_ = 1;
+    _fields_ = [("systm", tagSYSTEMTIME),
+               ("station", c_ulong),
+               ("channel", c_ulong),
+               ("device", c_ulong),
+               ("loghandle", c_ulong),
+               ("nCompressed", c_ulong),
+               ("hdrfilelen", c_ulong),
+               ("hdrfile", c_char_p),
+               ("cfgfilelen", c_ulong),
+               ("cfgfile", c_char_p),
+               ("datfilelen", c_ulong),
+               ("datfile", c_char_p),
+               ("inffilelen", c_ulong),
+               ("inffile", c_char_p)];
--- PyDllTest/src/datacacheAccessDllImporter.py
+++ PyDllTest/src/datacacheAccessDllImporter.py
@@ -42,25 +42,27 @@ class CDataCacheAccessImporter:
         return self.hDll.DCA_GetOldestID(pObj);
     
     # 根所uid读DataCache数据
-    def DCA_Read_by_uid(self, pObj, pDats, stalogid, needread, readedRef, pIdx, \
+    def DCA_Read_by_uid(self, pObj, pDats, stalogid, numNeedRead, numReaded, pIdx, \
                         pAddonbuf, addonbuflen):
-        self.hDll.DCA_Read_by_uid(pObj, pDats, stalogid, needread, byref(readedRef), \
+        self.hDll.DCA_Read_by_uid(pObj, pDats, stalogid, numNeedRead, byref(numReaded), \
                                          pIdx, pAddonbuf, addonbuflen)
 
-if __name__ == "__main__":
-    dcaObj = CDataCacheAccessImporter("D:\CET_P35\Common");
-    # 定时记录接口调用测试
+
+'''
+Test Case
+'''      
+ # 定时记录读取接口调用测试 
+def readDatalogTest(dllObj):
     pDatalogObj = c_void_p(None);
 
-    dcaObj.DCA_Initialize(pDatalogObj, "D:\CET_P35\Common", 1, DATACACHE_TYPE_DATALOG, \
-                         DATACACHE_DATALOG_IDXFILELEN, DATACACHE_DATALOG_DATFILELEN, GENERIC_READ);
-    ullNewestID = dcaObj.DCA_GetNewestID(pDatalogObj);
-    ullOldestID = dcaObj.DCA_GetOldestID(pDatalogObj);
+    dllObj.DCA_Initialize(pDatalogObj, "D:\CET_P35\Common", 1, DATACACHE_TYPE_DATALOG, \
+             DATACACHE_DATALOG_IDXFILELEN, DATACACHE_DATALOG_DATFILELEN, GENERIC_READ);
+    ullNewestID = dllObj.DCA_GetNewestID(pDatalogObj);
+    ullOldestID = dllObj.DCA_GetOldestID(pDatalogObj);
     
     # 从DataCache中取定时记录
-    NeedRead = 100;
-    datalog_Readed = c_ulong(0);
-    datalog_NeedRead = c_ulong(10);
+    numReaded = c_ulong(0);
+    numNeedRead = c_ulong(100);
     TArrDatalog = tagDATALOG * MAX_RWLOGCNT;
     arrDatalog = TArrDatalog();
     pArrDatalog = POINTER(tagDATALOG)();
@@ -70,13 +72,77 @@ if __name__ == "__main__":
     pArrDatalogIdx = POINTER(tagDTSNINDEX)();
     pArrDatalogIdx.contents = arrDatalogIdx;
     
-    print sizeof(tagDATALOG)
-    
     datalog_addonbuf = c_void_p(None);
     datalog_addonbuflen = c_ulong(0);
-    dcaObj.DCA_Read_by_uid(pDatalogObj, pArrDatalog, ullOldestID, datalog_NeedRead, \
-                           datalog_Readed, pArrDatalogIdx, datalog_addonbuf, datalog_addonbuflen);
-    print datalog_Readed.value;
-    print ullNewestID, ullOldestID;
+    dllObj.DCA_Read_by_uid(pDatalogObj, pArrDatalog, ullOldestID, numNeedRead, \
+           numReaded, pArrDatalogIdx, datalog_addonbuf, datalog_addonbuflen);
+                           
+    # 显示查询的结果
+    for i in range(numReaded.value):
+        print pArrDatalog[i].systm.wYear;
     
-    dcaObj.DCA_Finalize(pDatalogObj);
+    dllObj.DCA_Finalize(pDatalogObj);
+
+# 事件记录读取接口调用测试
+def readEventlogTest(dllObj):
+    pEventlogObj = c_void_p(None);
+    dllObj.DCA_Initialize(pEventlogObj, "D:\CET_P35\Common", 1, DATACACHE_TYPE_EVENTLOG, \
+            DATACACHE_EVENTLOG_IDXFILELEN, DATACACHE_EVENTLOG_DATFILELEN, GENERIC_READ);
+    ullNewestID = dllObj.DCA_GetNewestID(pEventlogObj);
+    ullOldestID = dllObj.DCA_GetOldestID(pEventlogObj);
+
+    # 从DataCache中获取事件记录
+    numReaded = c_ulong(0);
+    numNeedRead = c_ulong(100);
+    TArrEventlog = tagEVENTLOG * MAX_RWLOGCNT;
+    arrEventlog = TArrEventlog();
+    pArrEventlog = POINTER(tagEVENTLOG)();
+    pArrEventlog.contents = arrEventlog;
+    TArrEventlogIdx = tagDTSNINDEX * MAX_RWLOGCNT;
+    arrEventlogIdx = TArrEventlogIdx();
+    pArrEventlogIdx = POINTER(tagDTSNINDEX)();
+    pArrEventlogIdx.contents = arrEventlogIdx;
+
+    eventlog_addonbuf = c_void_p(None);
+    eventlog_addonbuflen = c_ulong(0);
+    dllObj.DCA_Read_by_uid(pEventlogObj, pArrEventlog, ullOldestID, numNeedRead, \
+            numReaded, pArrEventlogIdx, eventlog_addonbuf, eventlog_addonbuflen);
+
+    print sizeof(tagEVENTLOG);
+    # 显示查询的结果
+#     for i in range(numReaded.value):
+#         print str(pArrEventlog[i].time) + "." + str(pArrEventlog[i].msec);
+
+    dllObj.DCA_Finalize(pEventlogObj);
+
+# 波形记录读取接口调用测试   
+def readWformTest(dllObj):
+    pWformlogObj = c_void_p(None);
+    dllObj.DCA_Initialize(pWformlogObj, "D:\CET_P35\Common", 1, DATACACHE_TYPE_WFORMLOG, \
+            DATACACHE_WFORMLOG_IDXFILELEN, DATACACHE_WFORMLOG_DATFILELEN, GENERIC_READ);
+    ullNewestID = dllObj.DCA_GetNewestID(pWformlogObj);
+    ullOldestID = dllObj.DCA_GetOldestID(pWformlogObj);
+    
+    # 从DataCache中获取波形记录，一次只取一条波形
+    numReaded = c_ulong(0);
+    numNeedRead = c_ulong(1);
+    pCommtradeBuff = create_string_buffer(MAX_COMTRADE_FILELEN);
+    TWformlog = tagWFORMLOG * 1;
+    aWformlog = TWformlog();
+    pWformlog = POINTER(tagWFORMLOG)();
+    pWformlog.contents = aWformlog;
+    TArrWformlogIdx = tagDTSNINDEX * MAX_RWLOGCNT;
+    arrWformlogIdx = TArrWformlogIdx();
+    pArrWformlogIdx = POINTER(tagDTSNINDEX)();
+    pArrWformlogIdx.contents = arrWformlogIdx;
+    
+    
+    dllObj.DCA_Read_by_uid(pWformlogObj, byref(aWformlog), ullOldestID, 1, \
+            numReaded, pArrWformlogIdx, pCommtradeBuff, sizeof(c_ubyte) * MAX_COMTRADE_FILELEN);
+    print numReaded;
+
+if __name__ == "__main__":
+    dllObj = CDataCacheAccessImporter("D:\CET_P35\Common");
+    readDatalogTest(dllObj);
+    readEventlogTest(dllObj);
+    readWformTest(dllObj);
