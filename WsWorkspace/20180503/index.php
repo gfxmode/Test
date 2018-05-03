90fbd88a9967c19a345b0545e07206c54ccae1cf
gfxmode <gfxmode@live.com>
2018-05-04 00:00:39 +0800

    Init提交

--- WsWorkspace/20180503/index.php
+++ WsWorkspace/20180503/index.php
@@ -2,7 +2,7 @@
     require_once('./config/IniHandler.php');
     $objIni = new IniHandler('./config/config.ini');
     function getGlobalKey($in_key) {
-        global $objIni;
+        global $objIni;das
         echo ($objIni->getGlobalKey($in_key));
     }
 ?>
