d007933235e8712ccd4a242633768cf7ac555fbe
gfxmode <gfxmode@live.com>
2018-05-09 22:38:50 +0800

    更新，按日期保存图片

--- WsWorkspace/20180503/administrator/script/uploadImg.php
+++ WsWorkspace/20180503/administrator/script/uploadImg.php
@@ -2,21 +2,26 @@
     $extensions = array("jpg","bmp","gif","png");
     $uploadFilename = $_FILES['upload']['name'];
     $uploadFilesize = $_FILES['upload']['size'];
-    if($uploadFilesize  > 1024*2*1000){
+    if ($uploadFilesize  > 1024*2*1000) {
          echo "<font color=\"red\"size=\"2\">*图片大小不能超过2M</font>";
          exit;
     }
 
     $extension = pathInfo($uploadFilename, PATHINFO_EXTENSION);
-    if(in_array($extension,$extensions)){
+    if (in_array($extension, $extensions)) {
         $uploadPath ="../../uploadfile/";
+        $rdate = date("Ym", time());
+        $uploadPath .= $rdate . "/";
+        if (!is_dir($uploadPath)) {
+            mkdir($uploadPath);
+        }
         $uuid = str_replace('.','',uniqid("",TRUE)).".".$extension;
         $desname = $uploadPath.$uuid;
-        $previewname = '../../uploadfile/'.$uuid;
-        $tag = move_uploaded_file($_FILES['upload']['tmp_name'],$desname);
+        $previewname = $uploadPath.$uuid;
+        $tag = move_uploaded_file($_FILES['upload']['tmp_name'], $desname);
         $callback = $_REQUEST["CKEditorFuncNum"];
         echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction($callback,'".$previewname."','');</script>";
-    }else{
+    } else {
         echo "<font color=\"red\"size=\"2\">*文件格式不正确（必须为.jpg/.gif/.bmp/.png文件）</font>";
     }
 ?>
