452f4992bb0fbcb90a51f92619065695ed1c1f0d
gfxmode <gfxmode@live.com>
2018-05-13 11:51:34 +0800

    提交文章编辑器更新

--- WsWorkspace/20180503/administrator/script/saveArticle.php
+++ WsWorkspace/20180503/administrator/script/saveArticle.php
@@ -16,10 +16,15 @@
     }
     // 保存文章正文
     file_put_contents("../articles/art_".$idxLastArt.".html", $_POST["ckeditor"]);
-
+    // 获取文章第1张图为缩略图
+    preg_match('/<img.+src=\"?(.+\.(jpg|gif|bmp|bnp|png))\"?.+>/i', $_POST["ckeditor"], $match);
+    $imgPath = "";
+    if (count($match) > 1) {
+        $imgPath = $match[1];
+    }
     // 文章整体配置信息更新
-    $format = "%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%d";
-    $artItemValue = sprintf($format, $_POST["column"], $_POST["title"], $_POST["description"], $_POST["keywords"], time());
+    $format = "%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%s".INI_ART_ITEM_SPLIT_SYMBOL."%d".INI_ART_ITEM_SPLIT_SYMBOL."%s";
+    $artItemValue = sprintf($format, $_POST["column"], $_POST["title"], $_POST["description"], $_POST["keywords"], time(), $imgPath);
     $objIniSecArt = $objIni->getSection("Art");
     $objIniSecArt->addItem("Item".$idxLastArt, $artItemValue);
     $objIni->write();
